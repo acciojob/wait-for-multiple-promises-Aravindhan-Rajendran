cf74c45ad43754fe8adb773b1dc45b916a4c372c
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-08 18:54:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+function randomtime(){
+	return Math.floor(Math.random()*3000)+1000;
+}
 const Promise1 = new Promise(function(resolve, reject){
-	setTimeout(()=>{resolve('Promise 1 resolved'),2000});
+	let time=randomtime();
+	setTimeout(()=>{resolve(time),time});
 });
 const Promise2 = new Promise(function(resolve, reject){
-	setTimeout(()=>{resolve('Promise 2 resolved'),1000});
+	setTimeout(()=>{resolve(time),time});
 });
 const Promise3 = new Promise(function(resolve, reject){
-	setTimeout(()=>{resolve('Promise 3 resolved'),3000});
+	setTimeout(()=>{resolve(time),time});
 });
 
 Promise.all([Promise1,Promise2,Promise3]).then(total => {console.log(total)})
