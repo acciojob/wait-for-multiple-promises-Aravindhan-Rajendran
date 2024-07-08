6addc89b767d82dbccf71ef431f39d61b38f2f07
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-08 08:14:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+const Promise1 = new Promise(function(resolve, reject){
+	setTimeout(()=>{resolve('Promise 1 resolved'),2000});
+});
+const Promise2 = new Promise(function(resolve, reject){
+	setTimeout(()=>{resolve('Promise 2 resolved'),1000});
+});
+const Promise3 = new Promise(function(resolve, reject){
+	setTimeout(()=>{resolve('Promise 3 resolved'),3000});
+});
+
+Promise.all([Promise1,Promise2,Promise3]).then(total => {console.log(total)})
